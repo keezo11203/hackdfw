aec555a7c9fd919958af7b7a159183b3febddb0f
keezo11203 <91761833+keezo11203@users.noreply.github.com>
2021-10-02 14:52:23 -0400

    Update hack.cpp

--- hack.cpp
+++ hack.cpp
@@ -20,11 +20,39 @@ string Name;
   cout << " On a scale of 1 to 10, How are you feeling today? "<< endl;
   cin >> mental; 
  if ( mental < 4 ) {
-   cout << " Oh No! Here are some tips to feel better: " << endl;
+   cout << " Oh No! Here are some things I do to feel better: " << endl;
  }
 
    ifstream reader;//looks for text file
     reader.open("file.txt");//opens text file
-  
+    string word;
+    string word2;
+    string word3;
+    string word4;
+    string word5;
+    string word6;
+    string word7;
+    string word8;
+    string word9;
+    string word10;
+    string word11;
+    string word12;
+    string word13;
+ reader >> word >> word2 >> word3 >> word4 >> word5 >> word6 >> word7 >> word8 >> word9 >> word10 >> word11 >> word12 >> word13;
+ std::cout << word << std::endl;
+ std::cout << word2 << std::endl;
+ std::cout << word3 << std::endl;
+ std::cout << word4 << std::endl;
+ std::cout << word5 << std::endl;  
+ std::cout << word6 << std::endl;  
+ std::cout << word7 << std::endl;  
+ std::cout << word8 << std::endl;  
+ std::cout << word9 << std::endl;  
+ std::cout << word10 << std::endl;  
+ std::cout << word11 << std::endl;  
+ std::cout << word12 << std::endl;  
+ std::cout << word13 << std::endl; 
+   
+   
   return 0; 
 }

7a9ad4eb9071abef689fab56c23d5ced21ea4817
keezo11203 <91761833+keezo11203@users.noreply.github.com>
2021-10-02 14:32:52 -0400

    Update hack.cpp

--- hack.cpp
+++ hack.cpp
@@ -1,33 +1,30 @@
-#include <fstream> //file stream
-#include <string> //creates string variables
-#include <iostream> //input-output stream
-#include <iomanip> //lets you use setw
-using namespace std; //uses cout throughout program
-//cout means counsle out
-//cin means console in
-//endl means end line
+#include <fstream>
+#include <string> 
+#include <iostream> 
+#include <iomanip> 
+using namespace std; 
 
-void printWelcome(string Name){ //creates parameter
-   cout << " Hello " << setw(5) << Name; //prints
+void printWelcome(string Name){ 
+   cout << " Hello " << setw(5) << Name; 
 }
 
-int main()//tells computer this is a C++ program
+int main()
 {
-string Name; //creates string
-   cout << "Enter your first name: "; //gets name from user
-   cin >> Name;//prints
-   printWelcome(Name);//calls back to function 
+string Name;
+   cout << "Enter your first name: "; 
+   cin >> Name;
+   printWelcome(Name); 
    
    int mental;
-   int reason;
    
-  cout << " On a scale of 1 to 10, How are you feeling today?: "<< endl;
+  cout << " On a scale of 1 to 10, How are you feeling today? "<< endl;
   cin >> mental; 
- if ( mental < 4 ) 
-   cout << " Oh No! Why is that?" << endl; // print message
-   cin >> reason;
+ if ( mental < 4 ) {
+   cout << " Oh No! Here are some tips to feel better: " << endl;
  }
 
+   ifstream reader;//looks for text file
+    reader.open("file.txt");//opens text file
   
-  return 0; // tells the computer everything 
+  return 0; 
 }

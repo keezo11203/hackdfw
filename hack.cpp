2ae235991a370a491f266eecccbc28cb52254f54
keezo11203 <91761833+keezo11203@users.noreply.github.com>
2021-10-02 15:25:57 -0400

    Update hack.cpp

--- hack.cpp
+++ hack.cpp
@@ -1,3 +1,12 @@
+#include <fstream> //file stream
+#include <string> //creates string variables
+#include <iostream> //input-output stream
+#include <iomanip> //lets you use setw
+using namespace std; //uses cout throughout program
+//cout means counsle out
+//cin means console in
+//endl means end line
+
 #include <fstream>
 #include <string> 
 #include <iostream> 
@@ -5,7 +14,7 @@
 using namespace std; 
 
 void printWelcome(string Name){ 
-   cout << " Hello " << setw(5) << Name; 
+   cout << " Hello " << setw(3) << Name; 
 }
 
 int main()
@@ -19,10 +28,8 @@ string Name;
    
   cout << " On a scale of 1 to 10, How are you feeling today? "<< endl;
   cin >> mental; 
- if ( mental < 4 ) {
-   cout << " Oh No! Here are some things I do to feel better: " << endl;
- }
-
+ if ( mental <= 4  ) {
+   cout << "Oh No! Here are some things I do to feel better: " << endl;
    ifstream reader;//looks for text file
     reader.open("file.txt");//opens text file
     string word;
@@ -39,20 +46,24 @@ string Name;
     string word12;
     string word13;
  reader >> word >> word2 >> word3 >> word4 >> word5 >> word6 >> word7 >> word8 >> word9 >> word10 >> word11 >> word12 >> word13;
- std::cout << word << std::endl;
- std::cout << word2 << std::endl;
- std::cout << word3 << std::endl;
- std::cout << word4 << std::endl;
- std::cout << word5 << std::endl;  
- std::cout << word6 << std::endl;  
- std::cout << word7 << std::endl;  
- std::cout << word8 << std::endl;  
- std::cout << word9 << std::endl;  
- std::cout << word10 << std::endl;  
+ reader >> word >> word2 >> word3 >> word4 >> word5 >> word6 >> word7 >> word8 >> word9 >> word10 >> word11 >> word12 >> word13;
+ std::cout << word << setw(3) << word2 << setw(7) << word3 << std::endl;
+ std::cout << word4 << setw(3) << word5 << setw(3) << word6 << setw(7) << word7 << std::endl;  
+ std::cout << word8 << setw(7) << word9 << setw(7) << word10 << std::endl;  
  std::cout << word11 << std::endl;  
  std::cout << word12 << std::endl;  
  std::cout << word13 << std::endl; 
-   
-   
+std::cout << "Hope This Helps You Have a Better Day! " << std::endl; 
+ }
+
+   if ( mental >= 5 && mental <= 7 ) {
+   cout << "A calm, decent day I see. Hope things go your way today! " << endl;
+  }
+  
+  if ( mental >= 8 ) {
+   cout << " WOAH! save some joy for the rest of us will ya! Glad you're having a good day!" << endl;
+  }
+  
+  
   return 0; 
 }
